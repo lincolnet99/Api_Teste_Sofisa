28a49c408806356c21955695aff943b8f60bc72b
impacta.treinamentos <impacta.treinamentos@hotmail.com>
2021-02-13 19:30:05 -0300

    Commit de teste

--- Global.asax.cs
+++ Global.asax.cs
@@ -17,7 +17,6 @@ namespace Api_Teste_Sofisa
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
